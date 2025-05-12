aab0a0f9c1b353296cc8bd3a97260d2df942a0f7
Steve Inhun Ko <yoimono@naver.com>
2025-05-12 12:52:38 +0900

    pageable

--- src/main/java/kr/blug/tour/config/WebConfig.java
+++ src/main/java/kr/blug/tour/config/WebConfig.java
@@ -8,11 +8,24 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")  // 모든 요청 경로에 대해
-                .allowedOrigins("http://127.0.0.1:5500")  // 프론트엔드 도메인
-                .allowedMethods("*")  // GET, POST 등 모든 메서드 허용
-                .allowedHeaders("*")  // 모든 헤더 허용
-                .allowCredentials(true);  // 쿠키 허용 여부
-    }
+	  public void addCorsMappings(CorsRegistry registry) {
+
+	    registry.addMapping("/**")
+	            .allowedOrigins("*")
+	            .allowedMethods("HEAD", "GET", "POST", "PUT", "DELETE", "OPTIONS")
+	            .maxAge(300)
+	            .allowedHeaders("Authorization", "Cache-Control", "Content-Type");
+	  }
+    
+    
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")  // 모든 요청 경로에 대해
+//                .allowedOrigins("http://127.0.0.1:5500")  // 프론트엔드 도메인
+//                .allowedMethods("*")  // GET, POST 등 모든 메서드 허용
+//                .allowedHeaders("*")  // 모든 헤더 허용
+//                .maxAge(300)
+//                .allowCredentials(true);  // 쿠키 허용 여부   
+//    }
+    
+    
 }
--- src/main/java/kr/blug/tour/repository/FavoritesRepository.java
+++ src/main/java/kr/blug/tour/repository/FavoritesRepository.java
@@ -2,6 +2,9 @@ package kr.blug.tour.repository;
 
 import java.util.List;
 
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,9 +13,8 @@ import kr.blug.tour.entity.FavoritesEntity;
 @Repository
 public interface FavoritesRepository extends JpaRepository<FavoritesEntity, Long>{
 
-
-
-	List<FavoritesEntity> findAllByUser_UserId(Long userId);
-
+//	List<FavoritesEntity> findAllByUser_UserId(Long userId);
+	Page<FavoritesEntity> findAllByUser_UserId(Long userId, Pageable pageable);
+	
 	
 }
